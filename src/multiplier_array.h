1ef4d544edc0488f9046224d67cee4fe27f6c5cb
Кирилл Шахов <kirill.shakhov14@gmail.com>
2023-10-24 17:52:12 +0300

    feat: multiplier_array

---
+++ src/multiplier_array.h
@@ -0,0 +1,22 @@
+#include <systemc.h>
+
+SC_MODULE(MultiplierArray) {
+    static const int N_PORTS = 2;
+    sc_vector<sc_in<float>>  input1{"input1", N_PORTS};
+    sc_vector<sc_in<float>>  input2{"input2", N_PORTS};
+    sc_vector<sc_out<float>>  output{"output", N_PORTS};
+
+    void adder() {
+        for (int i = 0; i < N_PORTS; ++i) {
+            float result_value = input1[i].read() * input2[i].read();
+            output[i].write(result_value);
+        }
+    }
+
+    SC_CTOR(MultiplierArray) {
+        SC_METHOD(adder);
+        for (int i = 0; i < N_PORTS; ++i) {
+            sensitive << input1[i] << input2[i];
+        }
+    }
+};
