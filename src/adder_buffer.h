690dbcd7eef0aa8c65179750f48103be45fee419
Кирилл Шахов <kirill.shakhov14@gmail.com>
2023-10-24 16:53:29 +0300

    refactor: rename adder_buffer

--- src/adder_buffer.h
+++ src/adder_buffer.h
@@ -1,37 +1,26 @@
 #include <systemc.h>
 #include "./floating_point_adder.h"
 
-SC_MODULE(SumModule) {
+SC_MODULE(AdderBuffer) {
     sc_in_clk     clock;
     sc_in<bool>   reset;
-    sc_in<float> input1;
-    sc_in<float> input2;
+    sc_in<float>  input;
     sc_out<float> output;
 
-    FloatingPointAdder *adder1;
-
-    sc_signal<float> adder1Result;
-
-    sc_signal<float> intermediateResult;  // Для хранения промежуточных результатов.
+    sc_signal<float> intermediateResult;
 
     void sum () {
         if (reset.read()) {  // Асинхронный сброс
             output.write(0);
             intermediateResult.write(0.0);  // При сбросе обнуляем промежуточный результат.
         } else if (clock.posedge()) { // Если положительный фронт
-            float sum = adder1Result.read() + intermediateResult.read();  // Сложение текущего входа и промежуточного результата.
+            float sum = input.read() + intermediateResult.read();  // Сложение текущего входа и промежуточного результата.
             intermediateResult.write(sum);  // Сохранение нового промежуточного результата.
             output.write(sum);
         }
     }
 
-    SC_CTOR(SumModule) {
-        adder1 = new FloatingPointAdder("adder1");
-
-        adder1->input1(input1);
-        adder1->input2(input2);
-        adder1->result(adder1Result);
-
+    SC_CTOR(AdderBuffer) {
         SC_METHOD(sum);
         sensitive << reset;
         sensitive << clock.pos();  // Реагируем только на положительный фронт тактового сигнала
--- tests/adder_buffer_tb.cpp
+++ tests/adder_buffer_tb.cpp
@@ -1,50 +1,42 @@
 #include <systemc.h>
-#include "../src/summator.h"
+#include "../src/adder_buffer.h"
 #include "../src/clk.h"
 
 int sc_main (int argc, char* argv[]) {
     sc_signal<bool>   clock;
     sc_signal<bool>   reset;
-    sc_signal<float> in1;
-    sc_signal<float> in2;
+    sc_signal<float> in;
     sc_signal<float> out1;
 
     ClockGenerator clock_gen("clock_gen");
     clock_gen.clk(clock);
 
-    SumModule sumModule("SumModule");
-    sumModule.clock(clock);
-    sumModule.reset(reset);
-    sumModule.input1(in1);
-    sumModule.input2(in2);
-    sumModule.output(out1);
+    AdderBuffer adderBuffer("AdderBuffer");
+    adderBuffer.clock(clock);
+    adderBuffer.reset(reset);
+    adderBuffer.input(in);
+    adderBuffer.output(out1);
 
 
     sc_trace_file *wf = sc_create_vcd_trace_file("summator");
     sc_trace(wf, clock, "clock");
     sc_trace(wf, reset, "reset");
-    sc_trace(wf, in1, "in1");
-    sc_trace(wf, in2, "in2");
+    sc_trace(wf, in, "in");
     sc_trace(wf, out1, "out1");
 
     reset = 1;
-    in1   = 0;
-    in2   = 0;
+    in   = 0;
     out1  = 0;
     sc_start(10, SC_NS);
 
     reset = 0;
-    in1   = 1;
-    in2   = 2;
+    in   = 1;
     sc_start(10, SC_NS);
-    in1   = 7;
-    in2   = 9;
+    in   = 7;
     sc_start(10, SC_NS);
-    in1   = 3;
-    in2   = 6;
+    in   = 3;
     sc_start(10, SC_NS);
-    in1   = 5;
-    in2   = 2;
+    in   = 5;
     sc_start(100, SC_NS);
 
     sc_close_vcd_trace_file(wf);
